b7e0219d02ff834e0900a5dafbde38984f442062
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2022-04-10 10:53:12 -0600

    Add first visualizer

--- main.js
+++ main.js
@@ -1,11 +1,110 @@
 import preset from "https://luisarmando-testcoder.github.io/canvas-preset/index.js";
 
-var frecuencies;
-const { size, clear, draw, render, c, setAudioToggle } = preset();
+let frecuencies;
+let maxFrecuency;
+const { size, clear, draw, c, renderGroup } = preset();
 
 size();
 
-window.addEventListener("click", () => {
+const tableSquares = getTableSquares();
+const getFrecuency = (frecuency) =>
+  Math.round((frecuency / maxFrecuency) * 255);
+const getColor = (r, g, b) => `rgba(${r}, ${g}, ${b}, 1)`;
+
+draw(() => {
+  clear("black");
+
+  if (!frecuencies) return;
+
+  setFrecuencyIndexTableSquaresColors();
+
+  let flatTable = tableSquares.flat();
+  let mappedFlat = flatTable.map(({ x, y, color }) => {
+    const newSize = c.width / 10;
+    const newSpacing = 10;
+    const getPosition = (sideSize, axisPointer) => {
+      return (
+        axisPointer * (newSpacing / 2 + newSize) +
+        sideSize / 2 -
+        (tableSquares.length * (newSpacing / 2 + newSize)) / 2
+      );
+    };
+
+    return {
+      x: getPosition(c.width, x),
+      y: getPosition(c.height, y),
+      width: newSize,
+      height: newSize,
+      color
+    };
+  });
+
+  flatTable = null;
+
+  renderGroup("rect", mappedFlat);
+
+  mappedFlat = null;
+}, 10);
+
+function setFrecuencyIndexTableSquaresColors() {
+  tableSquares.forEach((row, y) => {
+    row.forEach((square, x) => {
+      square.color = getColor(
+        frecuencies[(y * 32) + x * 4 + 0],
+        frecuencies[(y * 32) + x * 4 + 1],
+        frecuencies[(y * 32) + x * 4 + 2],
+      );
+    });
+  });
+}
+
+function getTableSquares(sideSize = 8) {
+  return [...new Array(sideSize)].map((_, x) => {
+    return [...new Array(sideSize)].map((_, y) => {
+      return {
+        x,
+        y,
+        color: null,
+        width: 1,
+        height: 1,
+      };
+    });
+  });
+}
+
+function addEventListeners(eventNames, callback, target = window) {
+  eventNames.forEach((eventName) => {
+    target.addEventListener("click", callback);
+  });
+}
+
+function captureStream(stream) {
+  let audioContext = new AudioContext();
+  let microphone = audioContext.createMediaStreamSource(stream);
+  let analyser = audioContext.createAnalyser();
+
+  microphone.connect(analyser);
+
+  frecuencies = new Uint8Array(analyser.frequencyBinCount);
+
+  function play() {
+    analyser.getByteFrequencyData(frecuencies);
+
+    maxFrecuency = 0;
+
+    for (const frecuency of frecuencies) {
+      if (frecuency > maxFrecuency) {
+        maxFrecuency = frecuency;
+      }
+    }
+
+    requestAnimationFrame(play);
+  }
+
+  play();
+}
+
+addEventListeners(["click", "touchstart"], () => {
   if (frecuencies) return;
 
   navigator.getUserMedia =
@@ -13,49 +112,9 @@ window.addEventListener("click", () => {
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia;
 
-  navigator.getUserMedia({ video: false, audio: true }, callback, console.log);
+  navigator.getUserMedia(
+    { video: false, audio: true },
+    captureStream,
+    console.log
+  );
 });
-
-draw(() => {
-  if (!frecuencies) return;
-
-  // console.log(frecuencies);
-
-  const getRandom = () => Math.round(Math.random() * 10);
-
-  clear(`#${getRandom()}${getRandom()}${getRandom()}`);
-}, 10);
-
-function callback(stream) {
-  var ctx = new AudioContext();
-  var mic = ctx.createMediaStreamSource(stream);
-  var analyser = ctx.createAnalyser();
-  // oscillator will mimic heard frecuencies
-  // var osc = ctx.createOscillator();
-
-  mic.connect(analyser);
-  // osc.connect(ctx.destination);
-  // osc.start(0);
-
-  frecuencies = new Uint8Array(analyser.frequencyBinCount);
-
-  function play() {
-    analyser.getByteFrequencyData(frecuencies);
-
-    // get fullest bin
-    var index = 0;
-    for (var j = 0; j < analyser.frequencyBinCount; j++) {
-      if (frecuencies[j] > frecuencies[index]) {
-        index = j;
-      }
-    }
-
-    var frequency = (index * ctx.sampleRate) / analyser.fftSize;
-    // console.log(frequency);
-    // osc.frequency.value = frequency;
-
-    requestAnimationFrame(play);
-  }
-
-  play();
-}
